457e8d2b06b96eef5c21fde8d3fcdc576ef8a32e
Mdcetin <mehmethamdicetin@gmail.com>
2025-09-25 08:07:48 +0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,2 +1,9 @@
 import React from 'react'
-export default function App(){ return <div className='p-8 text-2xl'>Mehmet Hamdi Çetin - Ortopedi Landing</div> }
+
+export default function App() {
+  return (
+    <div className='p-8 text-2xl'>
+      Mehmet Hamdi Çetin - Ortopedi Landing
+    </div>
+  )
+}
