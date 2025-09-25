ff73d724286c64f9988d0704e644196de5c16dae
Mdcetin <mehmethamdicetin@gmail.com>
2025-09-25 08:08:30 +0300

    Update button.tsx

--- button.tsx
+++ button.tsx
@@ -1,2 +1,15 @@
 import React from 'react'
-export default function Button({children,...props}: React.ButtonHTMLAttributes<HTMLButtonElement>){return <button {...props} className='px-4 py-2 rounded-xl bg-slate-900 text-white'>{children}</button>}
+
+export default function Button({
+  children,
+  ...props
+}: React.ButtonHTMLAttributes<HTMLButtonElement>) {
+  return (
+    <button
+      {...props}
+      className='px-4 py-2 rounded-xl bg-slate-900 text-white'
+    >
+      {children}
+    </button>
+  )
+}
